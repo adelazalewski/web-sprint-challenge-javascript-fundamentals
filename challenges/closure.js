58db9f9ccfe78d5fb48e4bd7ec336be75fa24292
adelazalewski <paliuadela@yahoo.com>
2020-07-17 20:53:32 -0400

    finished prototype.js all tasks

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,21 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+//the nested function has closure= it finds the internal variable that it needs, in its lexical environment, the nested funtion and thats where it's lexcical environment is at as well. the internal variable has local scope and the nested function and reach and grab it without any trouble just like my function can reach for the external variable in the global scope as well. 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(number){
+  let count = 0;
+  return function add(){
+    for(let i = 1; i<=number; i++){
+      count = count + i;
+      
+    }
+    return count;
+  }
+    
+}
+
+console.log(summation(4));
